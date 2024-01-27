e163d8d36efc1cafb47728b76b2cc61fb8f7a0e4
Mian Al Ruhanyat <alruhanyat994@gmail.com>
2024-01-28 03:54:00 +0600

    adding pyfiglet

--- lab-5.py
+++ lab-5.py
@@ -3,6 +3,12 @@ import sys
 import urllib3
 from bs4 import BeautifulSoup
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+import pyfiglet
+
+# Display a banner using pyfiglet
+ascii_banner = pyfiglet.figlet_format("RuhanSec")
+print(ascii_banner)
+
 
 proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
 
