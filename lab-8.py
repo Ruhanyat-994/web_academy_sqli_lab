a907b4b651462fc76865c57dc7ef467038de7519
Mian Al Ruhanyat <alruhanyat994@gmail.com>
2024-02-03 06:47:44 +0600

    adding the banner

--- lab-8.py
+++ lab-8.py
@@ -3,6 +3,11 @@ import sys
 import urllib3
 from bs4 import BeautifulSoup
 import re
+import pyfiglet
+
+# Display a banner using pyfiglet
+ascii_banner = pyfiglet.figlet_format("RuhanSec")
+print(ascii_banner)
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
